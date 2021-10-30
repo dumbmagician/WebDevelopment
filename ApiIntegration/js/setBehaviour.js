53c2a8ebae26ffb36215be4c56ec58fce69ae1f1
Amit Rawat <pve9xq@splm.siemens.com>
2021-10-30 18:25:37 +0530

    updated code

---
+++ ApiIntegration/js/setBehaviour.js
@@ -0,0 +1,9 @@
+const CaptureFocus = (event)=>{
+    var element = document.getElementById('headerElement');
+    element.innerText = 'HI';   
+    var abc = confirm('Are you sure?');
+    console.log(abc);
+};
+
+//https://www.w3schools.com/howto/howto_css_icon_bar.asp
+//this project can be used to create basic builing blocks of a typical website
